a33c7275b383ba9cd596089ae6b28e44f96c3d05
Tony <gentilaj@mail.uc.edu>
2021-10-14 13:13:18 -0400

    Completed Project

--- src/PizzaOrderFrame.java
+++ src/PizzaOrderFrame.java
@@ -3,6 +3,7 @@ import javax.swing.border.TitledBorder;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 
 public class PizzaOrderFrame extends JFrame {
@@ -53,20 +54,25 @@ public class PizzaOrderFrame extends JFrame {
     double SubTotal;
 
     public PizzaOrderFrame() throws HeadlessException {
+        //initialize panels and set layout managers
         MainPanel = new JPanel(new BorderLayout());
         Organizer1 = new JPanel();
         Organizer1.setLayout(new BoxLayout(Organizer1, BoxLayout.Y_AXIS));
         Organizer2 = new JPanel();
+        //create the panels using their functions
         createCrustPanel();
         createSizePanel();
         createTopPanel();
         createRecPanel();
         createMenuPanel();
+        //add the pizza selection panels to organizer 1
         Organizer1.add(CrustPanel);
         Organizer1.add(SizePanel);
         Organizer1.add(TopPanel);
+        //add organizer 1 and the receipt panel to organizer 2
         Organizer2.add(Organizer1);
         Organizer2.add(RecPanel);
+        //add organizer 2 and the menu panel to the main panel
         MainPanel.add(Organizer2, BorderLayout.CENTER);
         MainPanel.add(MenuPanel, BorderLayout.SOUTH);
         add(MainPanel);
@@ -74,17 +80,21 @@ public class PizzaOrderFrame extends JFrame {
 
     private void createCrustPanel() {
         CrustPanel = new JPanel();
+        //create the button group
         CrustButtons = new ButtonGroup();
+        //Create the radio buttons and set Thin to the default selection
         Thin = new JRadioButton("Thin");
         Thin.setSelected(true);
         Regular = new JRadioButton("Regular");
         DeepDish = new JRadioButton("Deep-dish");
+        //Add the radio buttons to the button group and the panel
         CrustButtons.add(Thin);
         CrustButtons.add(Regular);
         CrustButtons.add(DeepDish);
         CrustPanel.add(Thin);
         CrustPanel.add(Regular);
         CrustPanel.add(DeepDish);
+        //create a border and add the border to the panel
         TitledBorder CrustBorder;
         CrustBorder = BorderFactory.createTitledBorder("Crust");
         CrustPanel.setBorder(CrustBorder);
@@ -93,10 +103,11 @@ public class PizzaOrderFrame extends JFrame {
 
     private void createSizePanel() {
         SizePanel = new JPanel();
+        //create the combo box using the size strings from the array
         SizeBox = new JComboBox(SizeStrings);
         SizeBox.setSelectedIndex(0);
-        //SizeBox.addActionListener();
         SizePanel.add(SizeBox);
+        //create a border and add the border to the panel
         TitledBorder SizeBorder;
         SizeBorder = BorderFactory.createTitledBorder("Size");
         SizePanel.setBorder(SizeBorder);
@@ -104,18 +115,21 @@ public class PizzaOrderFrame extends JFrame {
 
     private void createTopPanel() {
         TopPanel = new JPanel();
+        //create the check boxes with titles
         PepBox = new JCheckBox("Pepperoni");
         SauBox = new JCheckBox("Sausage");
         BanBox = new JCheckBox("Banana Peppers");
         TomBox = new JCheckBox("Tomato");
         PinBox = new JCheckBox("Pineapple");
         VanBox = new JCheckBox("Vanilla Pudding");
+        //add check boxes to the panel
         TopPanel.add(PepBox);
         TopPanel.add(SauBox);
         TopPanel.add(BanBox);
         TopPanel.add(TomBox);
         TopPanel.add(PinBox);
         TopPanel.add(VanBox);
+        //create a border and add the border to the panel
         TitledBorder TopBorder;
         TopBorder = BorderFactory.createTitledBorder("Toppings");
         TopPanel.setBorder(TopBorder);
@@ -123,27 +137,26 @@ public class PizzaOrderFrame extends JFrame {
 
     private void createRecPanel() {
         RecPanel = new JPanel();
-        RecDisplay = new JTextArea("",30,30);
+        //Create receipt display as a text area and set the font
+        RecDisplay = new JTextArea("",30,45);
         RecDisplay.setEditable(false);
-        RecDisplay.append("=========================================\n" +
-                    "Type of Crust & Size\t\tPrice\n" +
-                    "Ingredient \t\t\tPrice\n" +
-                    "\n" +
-                    "Sub-total: \t\t\tAmount\n" +
-                    "Tax:\t\t\tAmount\n" +
-                    "---------------------------------------------------------------------\n" +
-                    "Total: \t\t\tTotal\n" +
-                    "=========================================\n");
+        RecDisplay.setFont(new Font("Consolas", Font.PLAIN, 12));
+        //set default display text
+        RecDisplay.setText("Please place an order.");
+        //add text area to panel
         RecPanel.add(RecDisplay);
     }
 
     private void createMenuPanel() {
         MenuPanel = new JPanel();
+        //Create buttons and add listeners
         QuitButton = new JButton("Quit");
         QuitButton.addActionListener((ActionEvent ae) -> {System.exit(0);});
         ClearButton = new JButton("Clear");
+        ClearButton.addActionListener(new ClearListener());
         OrderButton = new JButton("Order");
         OrderButton.addActionListener(new OrderListener());
+        //Add Buttons to Panel
         MenuPanel.add(OrderButton);
         MenuPanel.add(ClearButton);
         MenuPanel.add(QuitButton);
@@ -152,9 +165,12 @@ public class PizzaOrderFrame extends JFrame {
     public class OrderListener implements ActionListener {
         @Override
         public void actionPerformed(ActionEvent e) {
-            //Check for Size
+            //Initialize variables needed
+            DecimalFormat df2 = new DecimalFormat("#.##");
             Object Size;
             String Crust = "";
+
+            //Check for Size
             Size = SizeBox.getSelectedItem();
             if (Size == "Small"){
                 BaseCost = 8.00;
@@ -189,7 +205,7 @@ public class PizzaOrderFrame extends JFrame {
 
             //Reset Toppings Array
             Toppings.clear();
-            //Check the toppings
+            //Check the toppings using if statements, add them to the array if button is checked
 
 
             if(PepBox.isSelected()){
@@ -211,22 +227,42 @@ public class PizzaOrderFrame extends JFrame {
                 Toppings.add("Vanilla Pudding");
             }
 
+            //Calculate Subtotal using the base price + the amount of toppings
             SubTotal = BaseCost + Toppings.size();
 
-//            RecDisplay.append("=========================================\n" +
-//                    "Type of Crust & Size\t\tPrice\n" +
-//                    "Ingredient \t\t\tPrice\n" +
-//                    "\n" +
-//                    "Sub-total: \t\t\tAmount\n" +
-//                    "Tax:\t\t\tAmount\n" +
-//                    "---------------------------------------------------------------------\n" +
-//                    "Total: \t\t\tTotal\n" +
-//                    "=========================================\n");
-            RecDisplay.removeAll();
-            RecDisplay.append("=========================================\n");
-            RecDisplay.append(String.format("%-50s",(Size.toString() + " " + Crust + " crust " + "Pizza")) + BaseCost);
+            //Build the receipt in the Receipt Display
+            RecDisplay.setText("=============================================\n");
+            RecDisplay.append(String.format("%-39s",(Size.toString() + " " + Crust + " Crust " + "Pizza")) + String.format("%6s",BaseCost) + "\n");
+            for(String t : Toppings){
+                RecDisplay.append(String.format("%-39s",t) + String.format("%6s","1.0") + "\n");
+            }
+            RecDisplay.append("\n" + String.format("%-39s","Sub-Total:") + String.format("%6s",SubTotal) + "\n");
+            RecDisplay.append(String.format("%-39s","Tax:") + String.format("%6s",df2.format(SubTotal * 0.07)) + "\n");
+            RecDisplay.append("---------------------------------------------\n");
+            RecDisplay.append(String.format("%-39s","Total:") + String.format("%6s",df2.format(SubTotal * 1.07)) + "\n");
+            RecDisplay.append("=============================================\n");
+        }
+    }
 
+    public class ClearListener implements ActionListener{
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            //Reset Crust to Thin which is the default
+            Thin.setSelected(true);
 
+            //Reset the size to Small which is default
+            SizeBox.setSelectedIndex(0);
+
+            //Reset all toppings to off which is default
+            PepBox.setSelected(false);
+            SauBox.setSelected(false);
+            BanBox.setSelected(false);
+            TomBox.setSelected(false);
+            PinBox.setSelected(false);
+            VanBox.setSelected(false);
+
+            //Reset receipt display to default text
+            RecDisplay.setText("Please place an order.");
         }
     }
 }
